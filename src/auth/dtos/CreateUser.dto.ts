4441aeaa3f077d2e4f1e259d74b02bcfe6225f0d
Tu <46893278+ABoyWithALaptop@users.noreply.github.com>
2022-09-13 19:20:29 +0700

    Implement register user endpoint

---
+++ src/auth/dtos/CreateUser.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, isNotEmpty, IsNotEmpty, MaxLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsEmail()
+    @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+    @MaxLength(32)
+  firstName: string;
+
+  @IsNotEmpty()
+    @MaxLength(32)
+  lastName: string;
+
+  @IsNotEmpty()
+    @MaxLength(32)
+  password: string;
+}
